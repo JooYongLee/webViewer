7a13cb81a47c01a487b9d02927a264709b85ced7
JooYongLee <juragon86@naver.com>
2021-11-17 01:03:05 +0900

    server example

---
+++ upload-download/server/src/middleware/upload.js
@@ -0,0 +1,23 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 100 * 1024 * 1024;
+
+// https://www.bezkoder.com/node-js-express-download-file/
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + "/resources/static/assets/uploads/");
+  },
+  filename: (req, file, cb) => {
+    console.log(file.originalname);
+    cb(null, file.originalname);
+  },
+});
+
+let uploadFile = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+}).single("file");
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
